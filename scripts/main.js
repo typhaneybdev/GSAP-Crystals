bd06b7db82af85893e710c5d931654a22fefbd03
Typhaney <typhaney@gmail.com>
2019-02-04 22:33:30 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,50 +1,94 @@
-//getting form input value
-function monthInput() {
-  let month = document.getElementById("month").value;
-  event.preventDefault();
-  switch (month) {
-    case "January":
-      text = "garnet is your birthstone";
-      break;
-    case "February":
-      text = "Amethyst is your birthstone";
-      break;
-    case "March":
-      text = "Aquamarine is your birthstone";
-      break;
-    case "April":
-      text = "Diamond is your birthstone";
-      break;
-    case "May":
-      text = "Emerald is your birthstone";
-      break;
-    case "June":
-      text = "Alexandrite & Pearl are your birthstones";
-      break;
-    case "July":
-      text = "Ruby is your birthstone";
-      break;
-    case "August":
-      text = "Peridot is your birthstone";
-      break;
-    case "September":
-      text = "Sapphire is your birthstone";
-      break;
-    case "October":
-      text = "Opal & Tourmaline are your birthstones";
-      break;
-    case "November":
-      text = "Citrine & Topaz are your birthstones";
-      break;
-    case "December":
-      text = "Tanzanite, Turquoise and Blue Zircon are your birthstones";
-      break;
+window.onload = function() {
+  //using timeline max from greesock library for animations; allowing animation stacking to be played sequentially
+  let tl = new TimelineMax({ repeat: 20, repeatDelay: 1 });
 
-    default:
-      "this is a test";
-  }
+  tl.from("#crystal_2", 1, { y: -100, opacity: 0, ease: Elastic.easeOut }).to(
+    "#crystal_2",
+    1,
+    { y: 200, opacity: 0, ease: Elastic.easeIn },
+    "+=1"
+  );
 
-  document.getElementById("submit").click(monthInput());
-  document.getElementById("test").innerHTML = text;
-  console.log(month);
-}
+  tl.to(
+    ".cls-2",
+    0.5,
+    { css: { stroke: "#f1c40f" }, ease: Linear.easeOut },
+    0.25
+  );
+  tl.to(
+    ".cls-2",
+    0.5,
+    { css: { stroke: "#2ecc71" }, ease: Linear.easeOut },
+    0.75
+  );
+  tl.to(
+    ".cls-2",
+    0.5,
+    { css: { stroke: "#3498db" }, ease: Linear.easeOut },
+    1.25
+  );
+
+  tl.to("#middle-section", 1, {
+    x: 100,
+    y: 100,
+    scale: 1.2,
+    rotation: 180
+  });
+
+  //animate crystal stroke color
+  tl.to(
+    ".cls-1",
+    0.5,
+    { css: { stroke: "#f1c40f" }, ease: Linear.easeOut },
+    0.25
+  );
+  tl.to(
+    ".cls-1",
+    0.5,
+    { css: { stroke: "#2ecc71" }, ease: Linear.easeOut },
+    0.75
+  );
+  tl.to(
+    ".cls-1",
+    0.5,
+    { css: { stroke: "#3498db" }, ease: Linear.easeOut },
+    1.25
+  );
+  tl.to(
+    ".cls-1",
+    0.5,
+    { css: { stroke: "#1fcc65" }, ease: Linear.easeOut },
+    1.5
+  );
+
+  tl.to(
+    ".cls-1",
+    0.5,
+    { css: { stroke: "#f9b132" }, ease: Linear.easeOut },
+    1.75
+  );
+
+  tl.to(".cls-1", 0.5, { css: { stroke: "#f4530c" }, ease: Linear.easeOut }, 2);
+
+  tl.to(
+    ".cls-1",
+    0.5,
+    { css: { stroke: "#f4530c" }, ease: Linear.easeOut },
+    2.25
+  );
+
+  tl.to(
+    ".cls-1",
+    0.5,
+    { css: { stroke: "#f4220c" }, ease: Linear.easeOut },
+    2.5
+  );
+
+  tl.to(
+    ".cls-1",
+    0.5,
+    { css: { stroke: "#7b1fcc" }, ease: Linear.easeOut },
+    2.75
+  );
+};
+
